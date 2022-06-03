b6421bcdf12ce709c5f04c37f3babce5591ccda3
Vinicius Santos <vinicius.santos@inmetrics.com.br>
2022-06-03 08:38:22 -0300

    Fim 4 aula - Router DOM Hooks

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,4 +1,5 @@
 import styles from './PaginaPadrao.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
 import { Outlet } from 'react-router-dom';
 
 export default function PaginaPadrao() {
@@ -9,7 +10,7 @@ export default function PaginaPadrao() {
           A casa do código e da massa
         </div>
       </header>
-      <div>
+      <div className={stylesTema.container}>
         <Outlet />
       </div>
     </>
