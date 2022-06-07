c2ac34da6fdd592494f3a4568888a6b6d8f899ba
Vinicius Santos <vinicius.santos@inmetrics.com.br>
2022-06-07 07:25:51 -0300

    Fim 6 aula - ajustes notfound e header

--- src/routes.tsx
+++ src/routes.tsx
@@ -17,9 +17,9 @@ export default function AppRouter() {
           <Route path='/' element={<PaginaPadrao />} >
             <Route index element={<Inicio />} />
             <Route path='cardapio' element={<Cardapio />} />
-            <Route path='sobre' element={<Sobre />} />     
-            <Route path='prato/:id' element={<Prato />} />      
+            <Route path='sobre' element={<Sobre />} />
           </Route>
+          <Route path='prato/:id' element={<Prato />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
         <Footer />
